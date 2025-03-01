8e42f26faeed86088a9570f695d5d7ccd8ad1464
NahuriraClintonie <clintonnahurira24@gmail.com>
2025-03-01 14:45:28 +0300

    Added a redirection functionality such that an authenticated page is redirected to the computers page

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -50,7 +50,7 @@ const RegisterForm = () => {
 
     try {
       const response = await axios.post(
-        " http://127.0.0.1:5000/user/register",
+        "https://delicate-factually-mastiff.ngrok-free.app/user/register",
         formDataToSend,
         {
           headers: {

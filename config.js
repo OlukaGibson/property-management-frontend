cb4f8f19c9c9a767928ca93a58ee90951c7532d1
OlukaGibson <gibsonoluka7@gmail.com>
2025-05-14 09:09:37 +0300

    done works

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     // baseURL: "http://127.0.0.1:5000"
     // baseURL: "https://delicate-factually-mastiff.ngrok-free.app"
-    baseURL: "http://35.202.217.87"
+    baseURL: "/api"
   };
   
   export default config;
